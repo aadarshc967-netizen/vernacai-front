20eb7ebce0a0c56a5577f94f32a42d64cd608be6
Aadarsh Chaudhary <aadarshc967@gmail.com>
2026-09-08 13:34:59 +0530

    Connect frontend to backend API

--- src/data/languages.ts
+++ src/data/languages.ts
@@ -4,7 +4,7 @@ export type Language = {
   code: string;
 };
 
-const API_URL = `http://${window.location.hostname}:8000`;
+const API_URL = import.meta.env.VITE_API_URL || `http://${window.location.hostname}:8000`;
 
 export const languages: Language[] = [];
 
